3b5468d4ecbbe6a3c3f49f867767d229ac650f9e
Mohammed Bellifa <mohammed.bellifa@gmail.com>
2015-10-29 22:10:23 -0600

    Added root check

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,8 +1,17 @@
 #!/bin/sh -ex
 
+#Root check 
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root." 1>&2
+  exit 1
+fi
+
 if [ -f /etc/debian_version ]; then
   ri_OS=Debian
   ri_OS_VER=$(cat /etc/debian_version)
 fi
 echo $ri_OS
 
+if [ "$ri_OS" = "Debian" ]; then
+  apt-get update
+fi
