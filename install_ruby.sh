f642ee0b0d1fedcf47b4570a016e6ab728d84718
Mohammed Bellifa <mohammed.bellifa@gmail.com>
2015-10-30 18:37:02 -0600

    Added bulk of ruby install for ubuntu

--- install_ruby.sh
+++ install_ruby.sh
@@ -17,22 +17,26 @@ if [ "$ri_OS" = "Debian" ]; then
     apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 fi
 
+
 cd
 if [ ! -d ".rbenv" ]; then
     git clone git://github.com/sstephenson/rbenv.git .rbenv
 fi
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+ri_RBENV_PATH="$HOME/.rbenv/bin"
+echo "$ri_RBENV_PATH/rbenv init -" >> ~/.bashrc
 . ~/.bashrc
-
-if [ ! -d "~/.rbenv/plugins/ruby-build" ]; then
+$ri_RBENV_PATH/rbenv init -
+if [ ! -d "$HOME/.rbenv/plugins/ruby-build" ]; then
     git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 fi 
 
+ri_RUBY_BUILD="$HOME/.rbenv/plugins/ruby-build/bin"
+
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
-rbenv install 2.2.3
-rbenv global 2.2.3
+"$ri_RBENV_PATH/rbenv" install 2.2.3
+"$ri_RBENV_PATH/rbenv" global 2.2.3
 
 ruby -v
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
